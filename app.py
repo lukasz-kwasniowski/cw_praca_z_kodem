e1afe99c69a0b02a36b14f378b2dbc9e2e3e134c
Jaukasz <109552414+lukasz-kwasniowski@users.noreply.github.com>
2023-05-14 16:53:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
- """
- COKOLWIEK2
- """
-return '<h1>Hello WSB! Greetings from Flask!</h1>'
+    """
+    COKOLWIEK2
+    """
+    return '<h1>Hello WSB! Greetings from Flask!</h1>'
 if __name__ == "__main__":
     app.run(debug=True)
